2ca012b560f19df6cf412f591525a5835fa76fbe
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-01 01:10:50 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -10,14 +10,14 @@ const App = () => {
     style={{ width: 420, height: 300, resizeMode: 'cover', position: 'relative' }}
     source={require("./assets/Tomatoes1.jpg")}/>
 
-    <View style={{ position: 'absolute', top: 60, left: 140, width: 250 }}>
-    <Text style={{ color: '#fff', textAlign: 'right', fontWeight: 'bold' }}>Icon</Text>
+    <View style={{ position: 'absolute', top: 60, paddingLeft: 10, paddingRight: 10, width: '100%' }}>
+    <Text style={{ color: '#fff', textAlign: 'right', fontWeight: 'bold', right: 15, bottom: 3 }}>Icon</Text>
 
     <View
-    style={{ flexDirection: 'row', backgroundColor: '#fff', paddingTop: 7, paddingBottom: 7, paddingLeft: 5 }}>
-    <Text style={{ marginRight: 10, color: 'rgb(140, 140, 134)', fontWeight: 'bold' }}>Icon</Text>
-    <TextInput placeholder="Search for Product" type="text" />
-    <Text>Icon</Text>
+    style={{ flexDirection: 'row', borderRadius: 10, backgroundColor: '#fff', justifyContent: 'space-between', paddingTop: 7, paddingBottom: 7, paddingLeft: 5, paddingRight: 5 }}>
+    <Text style={{ marginRight: 10, fontSize: 18, color: 'rgb(140, 140, 134)', fontWeight: 'bold' }}>Icon</Text>
+    <TextInput style={{ right: 60, fontSize: 16 }} placeholder="Search for Product" type="text" />
+    <Text style={{ marginLeft: 50, fontSize: 18, fontWeight: 'bold', color: 'rgb(140, 140, 134)' }}>Icon</Text>
     </View>
 
     <Text>VONS</Text>
