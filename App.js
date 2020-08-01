e542fecb5a618671fe8b335af79464d6729aeb21
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-01 00:46:54 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -11,8 +11,15 @@ const App = () => {
     source={require("./assets/Tomatoes1.jpg")}/>
 
     <View style={{ position: 'absolute', top: 60, left: 140, width: 250 }}>
-    <Text style={{ color: '#fff', textAlign: 'right'}}>Icon</Text>
+    <Text style={{ color: '#fff', textAlign: 'right', fontWeight: 'bold' }}>Icon</Text>
+
+    <View
+    style={{ flexDirection: 'row', backgroundColor: '#fff', paddingTop: 7, paddingBottom: 7, paddingLeft: 5 }}>
+    <Text style={{ marginRight: 10, color: 'rgb(140, 140, 134)', fontWeight: 'bold' }}>Icon</Text>
     <TextInput placeholder="Search for Product" type="text" />
+    <Text>Icon</Text>
+    </View>
+
     <Text>VONS</Text>
     <Text>Welcome Back, Loist</Text>
     </View>
