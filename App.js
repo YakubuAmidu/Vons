6668e558f7562b01d5edb3fac980316c4346cfa6
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-01 01:21:26 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -11,17 +11,17 @@ const App = () => {
     source={require("./assets/Tomatoes1.jpg")}/>
 
     <View style={{ position: 'absolute', top: 60, paddingLeft: 10, paddingRight: 10, width: '100%' }}>
-    <Text style={{ color: '#fff', textAlign: 'right', fontWeight: 'bold', right: 15, bottom: 3 }}>Icon</Text>
+    <Text style={{ color: '#fff', textAlign: 'right', fontWeight: 'bold', right: 15, bottom: 3, fontSize: 18 }}>Icon</Text>
 
     <View
-    style={{ flexDirection: 'row', borderRadius: 10, backgroundColor: '#fff', justifyContent: 'space-between', paddingTop: 7, paddingBottom: 7, paddingLeft: 5, paddingRight: 5 }}>
+    style={{ flexDirection: 'row', marginBottom: 30, borderRadius: 10, backgroundColor: '#fff', justifyContent: 'space-between', paddingTop: 7, paddingBottom: 7, paddingLeft: 5, paddingRight: 5 }}>
     <Text style={{ marginRight: 10, fontSize: 18, color: 'rgb(140, 140, 134)', fontWeight: 'bold' }}>Icon</Text>
     <TextInput style={{ right: 60, fontSize: 16 }} placeholder="Search for Product" type="text" />
     <Text style={{ marginLeft: 50, fontSize: 18, fontWeight: 'bold', color: 'rgb(140, 140, 134)' }}>Icon</Text>
     </View>
 
-    <Text>VONS</Text>
-    <Text>Welcome Back, Loist</Text>
+    <Text style={{ textAlign: 'center', fontSize: 35, fontWeight: '900', color: '#fff', marginBottom: 30 }}>VONS</Text>
+    <Text style={{ color: '#fff', fontSize: 20, textAlign: 'center'}}>Welcome Back, Loist!</Text>
     </View>
 
     </View>
